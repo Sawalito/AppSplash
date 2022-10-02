eff020a6d4e229553648a884797bffd418aaad78
Sawalito <rojas.vazquez.saul.ubaldo@gmail.com>
2022-10-02 17:35:11 -0500

    Segunda version

--- app/src/main/java/com/example/mysplash/Registro.java
+++ app/src/main/java/com/example/mysplash/Registro.java
@@ -2,7 +2,10 @@ package com.example.mysplash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Registro extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class Registro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        Button button5 = findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Registro.this, activity_login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
