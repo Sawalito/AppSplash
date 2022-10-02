2442158f47adeaaba1cff7e1d980e401ad1aeea0
Sawalito <rojas.vazquez.saul.ubaldo@gmail.com>
2022-10-02 16:53:50 -0500

    Primera version

---
+++ app/src/main/java/com/example/mysplash/activity_login.java
@@ -0,0 +1,27 @@
+package com.example.mysplash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class activity_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button button = findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(activity_login.this, Registro.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
