5f6ba9c03963f6d8a6e352a1c797f403115f832f
Sawalito <rojas.vazquez.saul.ubaldo@gmail.com>
2022-11-13 15:36:01 -0600

    Segunda modificacion

--- app/src/main/java/com/example/mysplash/menu.java
+++ app/src/main/java/com/example/mysplash/menu.java
@@ -4,20 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.mysplash.json.MyInfo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class menu extends AppCompatActivity {
+    private ListView listView;
+    private List<String> list;
     String aux;
-    MyInfo myInfo= null;
+    public static MyInfo myInfo= null;
     TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Object object= null;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        textView= findViewById(R.id.textViewM);
         Intent intent = getIntent();
         if(intent != null){
             if(intent.getExtras() !=null){
@@ -25,10 +31,18 @@ public class menu extends AppCompatActivity {
                 if (object != null) {
                     if (object instanceof MyInfo) {
                         myInfo = (MyInfo) object;
-                        textView.setText(myInfo.getUsuario());
+
                     }
                 }
             }
         }
+        listView = (ListView) findViewById(R.id.listViewId);
+        list = new ArrayList<String>();
+        for( int i = 0; i < 100; i++)
+        {
+            list.add( String.format( "Contraseña %d" , i + 1 ) );
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.activity_list_view,R.id.editText2, list );
+        listView.setAdapter(arrayAdapter);
     }
 }
